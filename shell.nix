b707f0caea9e8dc838fb0648491db113c0a9311c
Nickiel12 <nicholasyoungsumner@gmail.com>
2022-12-18 17:00:54 -0800

    first commit: added basic configuration files

---
+++ shell.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import <nixpkgs> {}}:
+
+let
+  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+  ruststable = (pkgs.latest.rustChannels.stable.default.override {
+      extensions = [
+        "rust-src"
+      ];
+    });
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    ruststable
+    sqlite
+    sqliteman
+    pkg-config
+    alsa-lib
+  ];
+
+  RUST_BACKTRACE = 1;
+
+  shellHook = ''
+    alias gust=/home/nixolas/Documents/Gust/target/debug/gust
+  '';
+}
